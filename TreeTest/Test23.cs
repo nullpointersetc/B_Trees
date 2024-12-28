368cdfa3b9536dfde0c548008e23ef395ca44df2
Darren Morby <nullpointersetc@users.noreply.github.com>
2024-12-28 04:00:38 -0500

    Create unit-test source file for 2-3 trees

---
+++ TreeTest/Test23.cs
@@ -0,0 +1,23 @@
+using Assert_t = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using Description_t = System.ComponentModel.DescriptionAttribute;
+using TestClass_t = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
+using TestMethod_t = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+
+namespace NullPointersEtc.TreeTests;
+
+[TestClass_t,
+    Description_t(description: "Tests for my 2-3 tree implementation.")]
+public sealed class TwoThreeTreeTests
+{
+    public TwoThreeTreeTests()
+    {
+    }
+
+    [TestMethod_t(displayName: "TwoThreeTree Tests")]
+    public void TestMethod1()
+    {
+        NullPointersEtc.TwoThreeFourTrees.TwoThreeFourTree<int, int> tree = new();
+
+        Assert_t.IsNotNull(tree, message: "tree should have been properly constructed");
+    }
+}
