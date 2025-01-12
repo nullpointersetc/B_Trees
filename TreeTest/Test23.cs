008c56a4864439315b6497e2e3fb7cbef5635a11
Darren Morby <nullpointersetc@users.noreply.github.com>
2025-01-12 05:01:00 -0500

    Minimal tests that pass

--- TreeTest/Test23.cs
+++ TreeTest/Test23.cs
@@ -1,7 +1,8 @@
-using Assert_t = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+﻿using Assert_t = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using Description_t = System.ComponentModel.DescriptionAttribute;
 using TestClass_t = Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute;
 using TestMethod_t = Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute;
+using TwoThreeTree_t = NullPointersEtc.TwoThreeTrees.TwoThreeTree<int, int>;
 
 namespace NullPointersEtc.TreeTests;
 
@@ -16,8 +17,10 @@ public sealed class TwoThreeTreeTests
     [TestMethod_t(displayName: "TwoThreeTree Tests")]
     public void TestMethod1()
     {
-        NullPointersEtc.TwoThreeFourTrees.TwoThreeFourTree<int, int> tree = new();
+        TwoThreeTree_t tree = new();
 
         Assert_t.IsNotNull(tree, message: "tree should have been properly constructed");
+
+        Assert_t.IsFalse(tree.Contains(5));
     }
 }
